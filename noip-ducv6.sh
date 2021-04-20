4e0168a75a665d53d6730e700ec38f271234e1b3
Charles Zhu <uciqiuxizhu@gmail.com>
2021-04-20 02:31:59 -0400

    Added fail-safe conditions for no addr and curl failure

--- noip-ducv6.sh
+++ noip-ducv6.sh
@@ -1,28 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-interface="INTERFACE e.g. enp3s0"
-user="EMAIL"
-pass="PASSWORD"
-hostname="HOSTNAME"
+interface='INTERFACE' # e.g. enp3s0
+user='EMAIL'
+pass='PASSWORD'
+hostname='HOSTNAME'
 
-url="https://dynupdate.no-ip.com/nic/update"
-agent="Personal noip-ducv6/linux-v1.0"
+url='https://dynupdate.no-ip.com/nic/update'
+agent='Personal noip-ducv6/linux-v1.0'
 
-lastaddr=''
+addrv4='0.0.0.0'
+lastaddr='::'
 
 update_ip () {
-    addr=$(ip -6 addr show dev $interface | sed -e'/inet6/,/scope global/s/^.*inet6 \([^ ]*\)\/.*scope global.*$/\1/;t;d')
-    if [[ $lastaddr != $addr ]]; then
-        echo "updating to $addr"
-        out=$(curl --get --silent --show-error --user-agent "$agent" --user "$user:$pass" -d "hostname=$hostname" -d "myipv6=$addr" $url)
+    addr=$(ip -6 addr show dev "${interface}" | sed -e'/inet6/,/scope global/s/^.*inet6 \([^ ]*\)\/.*scope global.*$/\1/;t;d')
+    if [[ -z ${addr} ]]; then
+        :
+    elif [[ ${lastaddr} != ${addr} ]]; then
+        echo "updating to ${addr}"
+        out=$(curl --get --silent --show-error --user-agent "${agent}" --user "${user}:${pass}" -d "hostname=${hostname}" -d "myip=${addrv4}" -d "myipv6=${addr}" ${url})
 
-        echo $out
+        echo ${out}
 
-        if [[ $out == nochg* ]] || [[ $out == good* ]]; then
-            lastaddr=$addr
-        elif [[ $out == 911 ]]; then
+        if [[ -z ${out} ]]; then
+            :
+        elif [[ ${out} == nochg* ]] || [[ ${out} == good* ]]; then
+            lastaddr=${addr}
+            sleep 3m
+        elif [[ ${out} == 911 ]]; then
             echo "911 response, waiting 30 minutes"
-            sleep 25m
+            sleep 28m
         else
             exit 1
         fi
@@ -30,6 +36,6 @@ update_ip () {
 }
 
 update_ip
-while sleep 5m; do
+while sleep 2m; do
     update_ip
 done
